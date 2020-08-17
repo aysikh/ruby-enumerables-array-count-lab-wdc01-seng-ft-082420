51936e5630ed2584a900f588b3b9032aaf5319a3
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-17 03:50:42 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -6,7 +6,12 @@ def count_strings(array)
 end
 
 
-
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+  empty_strings = []
+  array.count do |strings|
+    if strings.is_a? String do
+      empty_strings << array.count
+    end
+    empty_strings
 end
