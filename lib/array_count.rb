51bac31904efcaf448b27161f3ba996e53d35884
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-17 03:55:43 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
  array.count do |strings|
-   strings.is_a?(String)
+   strings.is_a?String
   end
 end
 
@@ -10,6 +10,5 @@ def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 
   array.count do { |strings| strings = ""}
-    strings.is_a? String
     end
 end
